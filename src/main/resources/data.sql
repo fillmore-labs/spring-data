9138c0b1562a15d17bf15e2e78b2117b6bc94733
Oliver Eikemeier <eikemeier@fillmore-labs.com>
2022-01-18 10:56:18 +0100

    Update dependencies
    Signed-off-by: Oliver Eikemeier <eikemeier@fillmore-labs.com>

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,14 @@
 INSERT INTO PERSON (ID, GIVEN_NAME, SURNAME)
-VALUES (1, 'Peter', 'Pan'),
-       (2, 'Wendy', 'Darling'),
-       (3, 'John', 'Darling'),
-       (4, 'Michael', 'Darling'),
-       (5, 'Nana', null);
+VALUES (101, 'Peter', 'Pan'),
+       (102, 'Wendy', 'Darling'),
+       (103, 'John', 'Darling'),
+       (104, 'Michael', 'Darling'),
+       (105, 'Nana', null);
 
 INSERT INTO EMAIL_ADDRESS (PERSON_ID, ADDRESS)
-VALUES (1, 'peter.pan@example.com'),
-       (1, 'peter.pan@example.org'),
-       (2, 'wendy.darling@example.com'),
-       (2, 'wendy.darling@example.org'),
-       (3, 'john.darling@example.com'),
-       (4, 'michael.darling@example.com');
+VALUES (101, 'peter.pan@example.com'),
+       (101, 'peter.pan@example.org'),
+       (102, 'wendy.darling@example.com'),
+       (102, 'wendy.darling@example.org'),
+       (103, 'john.darling@example.com'),
+       (104, 'michael.darling@example.com');
